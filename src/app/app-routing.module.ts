dd2880dd97f54d240f8e0b8dcae57cb8685adfc1
parimal sharma <parimalkumars@zignuts.com>
2023-07-12 14:46:51 +0530

    angular basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './MyComponent/about/about.component';
+import { TodosComponent } from './MyComponent/todos/todos.component';
 
-const routes: Routes = [];
-
+// const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'about', component: AboutComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'parimal';
+  // constructor(){
+  //   setTimeout(() => {
+  //     this.title = "sharma"
+  //   }, 3000);
+  // }
 }
